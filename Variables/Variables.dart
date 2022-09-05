85635bbcec4881dabbd3adf85b4c2524cb4b0e71
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-09-05 12:24:42 +0200

    3. Variables

---
+++ Variables/Variables.dart
@@ -0,0 +1,11 @@
+void main(){
+  var name = 'JJ';
+  print("Name " + name);
+  name = 'Javier';
+  print("Name " + name);
+
+  const name2 = 'JJ';
+  print("Name " + name2);
+  // name2 = 'Javier';
+  // print("Name " + name2);
+}
