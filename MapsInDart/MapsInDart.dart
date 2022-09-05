b1e3c70f0244dc17cd2084024dca7d7149aadb6e
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-09-05 14:13:24 +0200

    10. MapsInDart

---
+++ MapsInDart/MapsInDart.dart
@@ -0,0 +1,14 @@
+void main(){
+  test();
+}
+
+void test() {
+  var person = {'age':20 , 'name':'JJ'};
+  print(person['age']);
+  person['age'] = 23;
+  print(person['age']);
+
+  print(person['name']);
+  person['name'] = '23';
+  print(person['name']);
+}
