ee6eba8e9ebcd66dfa9e6292786a12883e8124ed
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-09-05 13:07:16 +0200

    7. Operators

--- Operators/Operators.dart
+++ Operators/Operators.dart
@@ -6,5 +6,6 @@ void test() {
   final age = 23;
   final halfOfAge = age / 2;
   final doubleOfAge = age * 2;
-  print(doubleOfAge.toString());
+  final ageMinusOne = --age;
+  print(ageMinusOne.toString());
 }
