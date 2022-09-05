4584df57e0ada1cb10a81cdad4a712a2b6786da7
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2022-09-05 16:04:57 +0200

    11. Enumerations

--- Enumerations/Enumerations.dart
+++ Enumerations/Enumerations.dart
@@ -1,3 +1,14 @@
-void main(){
+enum PersonProperties {firstName, lastName, age}
+enum AnimalType {dog, cat, bunny}
 
+void test(AnimalType animalType){
+  switch(animalType){
+    case AnimalType.dog: print("Is a dog");break;
+    case AnimalType.cat: print("Is a cat");break;
+    case AnimalType.bunny: print("Is a bunny");break;
+  }
+}
+
+void main(){
+  print(PersonProperties.firstName);
 }
